928ece2f6242a5397c20e3d4e52b942513ad03c0
Maximilian Beyer <mbeyer@M1.local>
2023-08-04 10:50:46 +0200

    formatting ec2

--- ec2.tf
+++ ec2.tf
@@ -1,30 +1,29 @@
 # Select newest AMI-id
 
-data "aws_ami" "nf_latest_linux_ami" {
-  most_recent = true
+data "aws_ami" "latest_linux_ami" {
+    most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
+    filter {
+        name   = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+        }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["amazon"] # Amazon
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+        }
+    owners = ["amazon"]
 }
 
-###Create EC2
-resource "aws_instance" "nf_webserver"{
-    #ami                    = data.aws_ami.nf_latest_linux_ami.id
+# Create EC2
+resource "aws_instance" "webserver"{
     ami = "ami-08541bb85074a743a"
     instance_type          = "t2.micro"
     key_name               = "vockey"
-    vpc_security_group_ids = [aws_security_group.nf_sg_http.id]
-    subnet_id              = aws_subnet.nf_publicsubnet1.id
+    vpc_security_group_ids = [aws_security_group.sg_http.id]
+    subnet_id              = aws_subnet.publicsubnet1.id
     tags = {
-        Name = "nf_webserver"
+        Name = "webserver"
         }
     user_data = file("userdata.sh")
     
