87b6c921f1045a632daf3a2a16903c7e3ace0deb
Maximilian Beyer <mbeyer@M1.local>
2023-08-04 16:53:53 +0200

    adding userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,45 +1,10 @@
 #!/bin/bash
 
-#DB_NAME="Wordpress-DB"
-#DB_USER="Maxey"
-#DB_PASSWORD="Yq5jk8tGXhP33kHM"
-#WP_PASSWORD="JH3zLgM34gm77tGZ"
+sudo yum update -y
 
+sudo yum install -y httpd mariadb-server php php-mysqlnd unzip
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
-#sudo yum update -y
-
-#sudo yum install -y httpd
-#sudo systemctl start httpd
-#sudo systemctl enable httpd
-
-#sudo yum install -y wget php-fpm php-mysqli php-json php-devel php
-
-#sudo systemctl start php-fpm
-#sudo systemctl enable php-fpm
-
-#sudo amazon-linux-extras enable php7.4
-#sudo yum clean metadata
-#sudo yum install -y php
-
-#sudo yum install -y mariadb-server
-#sudo systemctl start mariadb
-#sudo systemctl enable mariadb
-
-#wget https://wordpress.org/latest.tar.gz
-#tar -xzf latest.tar.gz
-
-#sudo mv wordpress/* /var/www/html/
-
-#mysql -u root -p -e "CREATE DATABASE \`$DB_NAME\` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci; GRANT ALL ON \`$DB_NAME\`.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD'; FLUSH PRIVILEGES;"
-
-#sudo chmod u+w /var/www/html/wp-config.php
-
-#sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-#sudo sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
-#sudo sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
-#sudo sed -i "s/password_here/$DB_PASSWORD/g" /var/www/html/wp-config.php
-
-#sudo chmod u-w /var/www/html/wp-config.php
-
-#sudo systemctl restart httpd
+cd /var/www/html
+aws s3 sync s3://deham6-wordpress/
